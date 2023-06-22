a5469dcc86f21bd2d3331679702de6b2a5a65b97
ADmi <alexander.starkin@gmail.com>
2023-06-22 19:51:16 +0300

    correct the mathces between picture, num and phrase of a weather type. Finished visual part

--- Pogona_parsing_tg_bot/Pogoda_bot_tg.py
+++ Pogona_parsing_tg_bot/Pogoda_bot_tg.py
@@ -1,12 +1,14 @@
 from aiogram import Bot, Dispatcher, executor, types
 
 import keyboards as kb
+import weather as wth
 
 API_TOKEN = '6117325444:AAGrLR9pVGX-y-nwmA6Nlr705NTShKxwMIY'
 # инициализация бота
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-cities = ['Москва', 'Санкт-Петербург', 'Нижний Новгород', 'Казань']
+city = ''
+period = ''
 
 
 # обработка запросов
@@ -22,9 +24,13 @@ async def send_welcome(message: types.Message):
 async def process_pogoda_command(message: types.Message):
     if message.text == 'Погода, серьезно?':
         await message.answer(text="Да, людишка, я могу показать следующие города:\n", reply_markup=kb.markup_cities)
-    print(message.text.strip())
-    if message.text[3:] in cities:
-        await message.answer(text="ну что ж, как скажешь, приятель\n", reply=kb.markup_weather_period)
+    if message.text[3:] in kb.cities:
+        city = message.text[3:]
+        await message.answer(text="ну что ж, как скажешь, приятель\n", reply_markup=kb.markup_weather_period)
+    if message.text in kb.weather_periods:
+        period = message.text
+        for el in wth.get_month():
+            await message.answer(el)
 
 
 # запуск кода
--- Pogona_parsing_tg_bot/weather.py
+++ Pogona_parsing_tg_bot/weather.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+import keyboards as kb
+
 url = "https://www.gismeteo.ru/weather-moscow-4368/month"
 response = requests.get(url, headers={
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
@@ -10,6 +12,7 @@ soup = BeautifulSoup(response.text, "lxml")
 all_wraps = soup.find("div", class_="widget widget-month").find_all(class_="row-item")
 cur_month = ''
 file = open("weather_response", "w")
+
 for wrap in all_wraps:
     mint = wrap.find(class_="mint").find(class_="unit unit_temperature_c").text
     maxt = wrap.find(class_="maxt").find(class_="unit unit_temperature_c").text
@@ -20,6 +23,8 @@ for wrap in all_wraps:
     file.write(date + ": " if len(date.split()) > 1 else date + str(' ' + cur_month) + ': ')
     print("минимальная температура: ", mint, "      максимальная температура: ", maxt)
     file.write("минимальная температура: " + str(mint) + "      максимальная температура: " + maxt + '\n')
+    pic_num = wrap.find("use").get("xlink:href")
+    print(pic_num + " " + str(pic_num in kb.pogoda_picture_num))
     # data = wrap.find(class_="date").text
     # time = wrap.find(class_="day").text
     # all_temps = wrap.find_all(class_="unit unit_temperature_c")
@@ -28,3 +33,31 @@ for wrap in all_wraps:
     # print(data, time,"  температура:",temp_mech, "по ощущению:" ,temp_feel)
 
 file.close()
+
+
+def get_month():
+    url = "https://www.gismeteo.ru/weather-moscow-4368/month"
+    response = requests.get(url, headers={
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+    })
+    soup = BeautifulSoup(response.text, "lxml")
+    response = []
+    all_wraps = soup.find("div", class_="widget widget-month").find_all(class_="row-item")
+    cur_month = ''
+    for wrap in all_wraps:
+        mint = wrap.find(class_="mint").find(class_="unit unit_temperature_c").text
+        maxt = wrap.find(class_="maxt").find(class_="unit unit_temperature_c").text
+        date = wrap.find(class_="date").text
+        pic_num = wrap.find("use").get("xlink:href")
+
+        if len(date.split()) > 1:
+            cur_month = date.split()[-1]
+        response.append(kb.pogoda_stikers[kb.pogoda_picture_num.index(pic_num)] + " " + kb.pogoda_phrase[
+            kb.pogoda_picture_num.index(pic_num)] + "\n")
+        response[-1] += (str(date + '\n' if len(date.split()) > 1 else date + str(' ' + cur_month + '\n')))
+        # print(date if len(date.split()) > 1 else date + str(' ' + cur_month), end=' ')
+        response[-1] += "минимальная температура:" + str(mint) + "\n" + "максимальная температура: " + str(
+            maxt) + "\n" + str(pic_num)
+        # print("минимальная температура: ", mint, "      максимальная температура: ", maxt)
+
+    return response
