07cfe22041603ba0c9f1f11be063cfd36633e1cb
ADmi <alexander.starkin@gmail.com>
2023-06-23 18:47:24 +0300

    secure added

--- Pogona_parsing_tg_bot/Pogoda_bot_tg.py
+++ Pogona_parsing_tg_bot/Pogoda_bot_tg.py
@@ -1,11 +1,11 @@
 from aiogram import Bot, Dispatcher, executor, types
 
 import keyboards as kb
+import secure as sc
 import weather as wth
 
-API_TOKEN = '6117325444:AAGrLR9pVGX-y-nwmA6Nlr705NTShKxwMIY'
 # инициализация бота
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=sc.API_TOKEN)
 dp = Dispatcher(bot)
 city = ''
 period = ''
@@ -29,8 +29,11 @@ async def process_pogoda_command(message: types.Message):
         await message.answer(text="ну что ж, как скажешь, приятель\n", reply_markup=kb.markup_weather_period)
     if message.text in kb.weather_periods:
         period = message.text
-        for el in wth.get_month():
-            await message.answer(el)
+        if period == "На месяц":
+            for el in wth.get_month():
+                await message.answer(el)
+        elif period == "На две недели":
+            wth.get_2week()
 
 
 # запуск кода
