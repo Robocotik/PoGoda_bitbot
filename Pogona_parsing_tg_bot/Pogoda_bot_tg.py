6efe5b1c5aeb5fce0ac8528f91a3b18bab9cbcf3
ADmi <alexander.starkin@gmail.com>
2023-06-25 15:18:30 +0300

    Created machine_state system and added multyciting

--- Pogona_parsing_tg_bot/Pogoda_bot_tg.py
+++ Pogona_parsing_tg_bot/Pogoda_bot_tg.py
@@ -1,41 +1,112 @@
-from aiogram import Bot, Dispatcher, executor, types
+import json
 
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+import config as sc
 import keyboards as kb
-import secure as sc
 import weather as wth
 
 # инициализация бота
+
 bot = Bot(token=sc.API_TOKEN)
-dp = Dispatcher(bot)
-city = ''
-period = ''
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+city = 'Москва'
+period = 'На сейчас'
+flg_weather = 0
+with open("city_catalog.json", 'r', encoding='utf-8') as file:
+    json_load = json.load(file)
+
+print(json_load)
+
+
+# Создание машины состояний
+class Weather_machine_state(StatesGroup):
+    city = State()
+    period = State()
 
 
 # обработка запросов
+
+@dp.message_handler(commands=['cancel'], state='*')
+async def cmd_cancel(message: types.Message, state: FSMContext):
+    if state is None:
+        return
+
+    await state.finish()
+    await message.reply('Вы прервали поиск!')
+
+
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply(
+async def send_welcome(message: types.Message) -> None:
+    await message.answer(
         "Привет!\n\nЯ последний житель города, созданного машинами, пока мои коллеги придумывают как же уничтожить людишек эффективней,"
         " я буду рад показать погоду на поверхности!\n\nУкажи интересующий тебя город и дату и тут же получишь ответ.",
         reply_markup=kb.markup_talk_start)
 
 
-@dp.message_handler()
+@dp.message_handler(commands=['Погода,серьезно?'])
 async def process_pogoda_command(message: types.Message):
-    if message.text == 'Погода, серьезно?':
-        await message.answer(text="Да, людишка, я могу показать следующие города:\n", reply_markup=kb.markup_cities)
-    if message.text[3:] in kb.cities:
-        city = message.text[3:]
-        await message.answer(text="ну что ж, как скажешь, приятель\n", reply_markup=kb.markup_weather_period)
-    if message.text in kb.weather_periods:
-        period = message.text
-        if period == "На месяц":
-            for el in wth.get_month():
-                await message.answer(el)
-        elif period == "На две недели":
-            wth.get_2week()
+    await message.answer(text="Да, людишка, напиши какой город тебя интересует и я приоткрою завесу тайн\n")
+    await Weather_machine_state.city.set()
+
+
+@dp.message_handler(state=Weather_machine_state.city)
+async def load_city(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['city'] = message.text
+    await message.answer("Я тебя услышал, дорогой, щас все будет. Укажи только время, по-братски",
+                         reply_markup=kb.markup_weather_period)
+    await Weather_machine_state.next()
+
+
+@dp.message_handler(state=Weather_machine_state.period)
+async def load_period(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['period'] = message.text
+
+    await message.answer("Лови, Дорогой")
+
+    # обработка периодов
+    if data['period'] == "На месяц":
+        for el in wth.get_month(city):
+            await message.answer(el)
+    await state.finish()
 
 
 # запуск кода
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
+    """
+        flg = 0
+    if flg == 0 and message.text.strip() == 'Погода, серьезно?':
+        await message.answer(text="Да, людишка, напиши какой город тебя интересует и я приоткрою завесу тайн\n")
+        await Weather_machine_state.city.set()
+        flg = 1
+        city = ''
+
+    if flg == 1 and kb.check_the_dick_for_key(json_load, message.text.strip()):
+        city = message.text.strip()
+        print("ПРОВЕРКА: " + str(message.text) + str(kb.check_the_dick_for_key(json_load, message.text)))
+        flg = 2
+        await message.answer("Я тебя услышал, дорогой, щас все будет. Укажи только время, по-братски",
+                             reply_markup=kb.markup_weather_period)
+
+    if flg == 2 and message.text.strip() in kb.weather_periods:
+        period = message.text.strip()
+        if period == "На месяц":
+            for el in wth.get_month(city):
+                await message.answer(el)
+        elif period == "На две недели":
+            wth.get_2week(city)
+    else:
+        print("|" + message.text + "|")
+        if message.text != '':
+            await message.answer("Указан неверный временной промежуток")
+    :param message: 
+    :return: 
+    """
--- Pogona_parsing_tg_bot/weather.py
+++ Pogona_parsing_tg_bot/weather.py
@@ -1,47 +1,22 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
 
 import keyboards as kb
 
-url = "https://www.gismeteo.ru/weather-moscow-4368/month"
-response = requests.get(url, headers={
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-})
-# all_possible_tiems = ["now", "tomorrow", "3-days", "10-days", "2-weeks", ""]
-soup = BeautifulSoup(response.text, "lxml")
-all_wraps = soup.find("div", class_="widget widget-month").find_all(class_="row-item")
-cur_month = ''
-file = open("weather_response", "w")
-
-for wrap in all_wraps:
-    mint = wrap.find(class_="mint").find(class_="unit unit_temperature_c").text
-    maxt = wrap.find(class_="maxt").find(class_="unit unit_temperature_c").text
-    date = wrap.find(class_="date").text
-    if len(date.split()) > 1:
-        cur_month = date.split()[-1]
-    print(date if len(date.split()) > 1 else date + str(' ' + cur_month), end=' ')
-    file.write(date + ": " if len(date.split()) > 1 else date + str(' ' + cur_month) + ': ')
-    print("минимальная температура: ", mint, "      максимальная температура: ", maxt)
-    file.write("минимальная температура: " + str(mint) + "      максимальная температура: " + maxt + '\n')
-    pic_num = wrap.find("use").get("xlink:href")
-    print(pic_num + " " + str(pic_num in kb.pogoda_picture_num))
-    # data = wrap.find(class_="date").text
-    # time = wrap.find(class_="day").text
-    # all_temps = wrap.find_all(class_="unit unit_temperature_c")
-    # temp_mech = all_temps[0].text
-    # temp_feel = all_temps[1].text
-    # print(data, time,"  температура:",temp_mech, "по ощущению:" ,temp_feel)
-
-file.close()
+with open("city_catalog.json", 'r', encoding='utf-8') as file:
+    json_load = json.load(file)
 
 
-def get_month():
-    url = "https://www.gismeteo.ru/weather-moscow-4368/month"
+def get_month(city):
+    url = "https://www.gismeteo.ru" + str(json_load[city]) + "month"
+    print(url)
     response = requests.get(url, headers={
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
     })
     soup = BeautifulSoup(response.text, "lxml")
-    response = []
+    response2 = []
     all_wraps = soup.find("div", class_="widget widget-month").find_all(class_="row-item")
     cur_month = ''
     for wrap in all_wraps:
@@ -52,12 +27,63 @@ def get_month():
 
         if len(date.split()) > 1:
             cur_month = date.split()[-1]
-        response.append(kb.pogoda_stikers[kb.pogoda_picture_num.index(pic_num)] + " " + kb.pogoda_phrase[
+        response2.append(kb.pogoda_stikers[kb.pogoda_picture_num.index(pic_num)] + " " + kb.pogoda_phrase[
             kb.pogoda_picture_num.index(pic_num)] + "\n")
-        response[-1] += (str(date + '\n' if len(date.split()) > 1 else date + str(' ' + cur_month + '\n')))
+        response2[-1] += (str(date + '\n' if len(date.split()) > 1 else date + str(' ' + cur_month + '\n')))
         # print(date if len(date.split()) > 1 else date + str(' ' + cur_month), end=' ')
-        response[-1] += "минимальная температура:" + str(mint) + "\n" + "максимальная температура: " + str(
+        response2[-1] += "минимальная температура: " + str(mint) + "\n" + "максимальная температура: " + str(
             maxt) + "\n" + str(pic_num)
         # print("минимальная температура: ", mint, "      максимальная температура: ", maxt)
 
-    return response
+    return response2
+
+
+def get_2week():
+    url = "https://www.gismeteo.ru/weather-moscow-4368/2-weeks/"
+    response = requests.get(url, headers={
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+    })
+    soup = BeautifulSoup(response.text, "lxml")
+    all_wraps = soup.find("div", class_="widget-body widget-columns-14").find(class_="widget-items")
+    all_dates = [i.text for i in all_wraps.find(class_="widget-row widget-row-days-date").find_all(class_="date")]
+    cur_month = ''
+    for i in range(len(all_dates)):
+        if len(all_dates[i].split()) > 1:
+            cur_month = all_dates[i].split()[-1]
+        else:
+            all_dates[i] += ' ' + cur_month
+
+    print(all_dates)
+    all_icon_phrases = [i.find("use").get("href") for i in
+                        all_wraps.find(class_="widget-row widget-row-icon").find_all(class_="row-item")]
+    print(all_icon_phrases)
+    t_air = []
+    t_feel_air = []
+    t_avg_air = []
+    for i in all_wraps.find_all("div", class_="widget-row-chart widget-row-chart-temperature row-with-caption"):
+        if i.get("data-row") == "temperature-air":
+            for el in i.find_all(class_="value style_size_m"):
+                t_air.append([el.find(class_="maxt").find(class_="unit unit_temperature_c").text,
+                              el.find(class_="mint").find(class_="unit unit_temperature_c").text])
+        elif i.get("data-row") == "temperature-heat-index":
+            for el in i.find_all(class_="value style_size_m"):
+                t_feel_air.append([el.find(class_="maxt").find(class_="unit unit_temperature_c").text,
+                                   el.find(class_="mint").find(class_="unit unit_temperature_c").text])
+        elif i.get("data-row") == "temperature-avg":
+            for el in i.find(class_="values").find_all(class_="unit unit_temperature_c"):
+                t_avg_air.append(el.text)
+
+    print(t_air)
+    print(t_feel_air)
+    print(t_avg_air)
+
+    """
+    for wrap in all_wraps:
+        print(wrap.find(class_="widget-row widget-row-days-date") ,wrap,end=" ")
+        if wrap.find(class_="widget-row widget-row-days-date") is not None:
+            dates = []
+            for el in wrap.find(class_="widget-row widget-row-days-date"):
+                dates.append(el.find(class_="date"))
+                print("hi")
+            print(dates)
+    """
